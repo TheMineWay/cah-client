16f68813e440415ecf9e48c7889bfd4e90b5c1fa
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-15 22:28:17 +0200

    i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { setupI18n } from "./utils/i18n/i18n-setup.util.ts";
 import "./index.css";
+
+setupI18n();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
---
+++ src/utils/i18n/i18n-setup.util.ts
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+// en_US
+import en_US_layout from "../../locales/en_US/platform/layout.json";
+
+export const setupI18n = () => {
+  const resources = {
+    [Languages.en_US]: {
+      [Translations.layout]: en_US_layout,
+    },
+  };
+
+  i18n.use(initReactI18next).init({ resources });
+};
+
+export enum Languages {
+  en_US = "en_US",
+}
+
+export enum Translations {
+  layout = "layout",
+  login = "login",
+}
