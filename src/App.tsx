707b0c234c60c8d1c9f480f2cdab12c89572e72f
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-21 20:38:05 +0200

    Change provider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { Button } from "react-daisyui";
-import Providers from "./providers/providers";
 
 export default function App() {
   return (
-    <Providers>
-      <>
-        <Button color="primary">Helo world</Button>
-      </>
-    </Providers>
+    <>
+      <Button color="primary">Helo world</Button>
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { setupI18n } from "./utils/i18n/i18n-setup.util.ts";
 import "./index.css";
+import Providers from "./providers/providers.tsx";
 
 setupI18n();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>
 );
