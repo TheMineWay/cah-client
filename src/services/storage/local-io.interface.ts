6ed8aa1008135539f0dd68f177ce9631ea58249a
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-22 12:07:24 +0200

    Uid

--- src/services/storage/local-io.interface.ts
+++ src/services/storage/local-io.interface.ts
@@ -1,22 +1,33 @@
-import { uuid } from '../../types/generic/uuid.type';
+import { uuid } from "../../types/basic/uuid.type";
 
 export type ILocalIOGetOptions = CommonOptions;
 export type ILocalIOSafeGetOptions = {
-    // In case fallback is used, store it as the item value.
-    saveFallback?: boolean;
+  // In case fallback is used, store it as the item value.
+  saveFallback?: boolean;
 } & CommonOptions;
 export type ILocalIOSetOptions = CommonOptions;
 export type ILocalIORemoveOptions = CommonOptions;
 
 export type CommonOptions = {
-    namespace?: string;
-    userNamespace?: uuid;
+  namespace?: string;
+  userNamespace?: uuid;
 };
 
 export interface ILocalIO {
-    get: <T extends Object>(key: string, options?: ILocalIOGetOptions) => T | null;
-    safeGet: <T extends Object>(key: string, fallback: T, options?: ILocalIOSafeGetOptions) => T;
-    set: <T extends Object = Object>(key: string, value: T, options?: ILocalIOSetOptions) => void;
-    remove: (key: string, options?: ILocalIORemoveOptions) => void;
-    clear: () => void;
+  get: <T extends Object>(
+    key: string,
+    options?: ILocalIOGetOptions
+  ) => T | null;
+  safeGet: <T extends Object>(
+    key: string,
+    fallback: T,
+    options?: ILocalIOSafeGetOptions
+  ) => T;
+  set: <T extends Object = Object>(
+    key: string,
+    value: T,
+    options?: ILocalIOSetOptions
+  ) => void;
+  remove: (key: string, options?: ILocalIORemoveOptions) => void;
+  clear: () => void;
 }
